2c8312051275db13b33033292a6b12cf321f31d5
Bjonne <Bjonne696@gmail.com>
2024-01-07 22:17:21 +0100

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { HeaderContainer } from '../styles/ContainerStyles';
 import { Navigation, NavList, NavItem } from '../styles/NavigationStyles';
 import CartIcon from './CartIcon';
 import { useSelector } from 'react-redux'; 
 
 const Header = () => {
+  const navigate = useNavigate();
   const cartItems = useSelector((state) => state.cart.items);
   const itemCount = cartItems.length;
+
+  const handleCartClick = () => {
+    navigate('/checkout'); 
+  };
 
   return (
     <HeaderContainer>
@@ -15,7 +20,7 @@ const Header = () => {
         <h1>ShopEase</h1>
         <NavList>
           <NavItem>
-            <CartIcon itemCount={itemCount} onClick={() => navigate('/checkout')} />
+            <CartIcon itemCount={itemCount} onClick={handleCartClick} />
           </NavItem>
           <NavItem>
             <Link to="/">ShopEase</Link>
